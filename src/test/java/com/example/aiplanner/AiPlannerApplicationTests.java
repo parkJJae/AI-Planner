6fd98d53fd4b0081642644b117528cac47071dee
parkjjae <wgpark0830@naver.com>
2025-02-23 00:31:34 +0900

    테스트

---
+++ src/test/java/com/example/aiplanner/AiPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aiplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
