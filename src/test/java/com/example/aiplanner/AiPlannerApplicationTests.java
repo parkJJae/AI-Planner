1c69ea1d0a1df4f308f2cfb61802c15c039e798b
parkJJae <wgpark0830@naver.com>
2025-02-24 21:20:02 +0900

    테스트

--- src/test/java/com/example/aiplanner/AiPlannerApplicationTests.java
+++ src/test/java/com/example/aiplanner/AiPlannerApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.aiplanner;
 
+import com.example.aiplanner.model.UserEntity;
+import com.example.aiplanner.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AiPlannerApplicationTests {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
-    void contextLoads() {
+    void testJpa() {
+        UserEntity user = new UserEntity();
+        user.setUseremail("wgpark0830@naver.com");
+        user.setUserpassword("1234");
+        this.userRepository.save(user);
+
     }
 
 }
