5e934b53d6f900d61084e456c538e4b572f740e2
parkJJae <wgpark0830@naver.com>
2025-02-24 21:20:26 +0900

    회원가입 entity

---
+++ src/main/java/com/example/aiplanner/model/UserEntity.java
@@ -0,0 +1,29 @@
+package com.example.aiplanner.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "userentity")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userid;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String useremail;
+
+    @Column(nullable = false)
+    private String userpassword;
+
+
+}
