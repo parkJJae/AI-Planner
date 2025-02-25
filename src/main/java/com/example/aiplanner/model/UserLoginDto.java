8ae2886cae6d05164552edde781512beb023860e
parkJJae <wgpark0830@naver.com>
2025-02-26 01:18:56 +0900

    로그인 DTO

---
+++ src/main/java/com/example/aiplanner/model/UserLoginDto.java
@@ -0,0 +1,15 @@
+package com.example.aiplanner.model;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserLoginDto {
+    @NotBlank(message = "이메일을 입력하세요")
+    private String useremail;
+
+    @NotBlank(message = "비밀번호를 입력하세요")
+    private String userpassword;
+}
