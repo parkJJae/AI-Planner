9b8cea2f7f8effb98570455fb3542106c1a0b67b
parkJJae <wgpark0830@naver.com>
2025-02-25 11:51:15 +0900

    서비스

--- src/main/java/com/example/aiplanner/service/UserService.java
+++ src/main/java/com/example/aiplanner/service/UserService.java
@@ -1,6 +1,8 @@
 package com.example.aiplanner.service;
 
 import com.example.aiplanner.model.UserEntity;
+import com.example.aiplanner.model.UserRegisterDto;
+import com.example.aiplanner.model.UserResponseDto;
 import com.example.aiplanner.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,12 +14,20 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public UserEntity Create(String username, String userpassword, String useremail){
-        UserEntity userEntity = new UserEntity();
-        userEntity.setUsername(username);
-        userEntity.setUseremail(useremail);
-        userEntity.setUserpassword(passwordEncoder.encode(userpassword));
-        this.userRepository.save(userEntity);
-        return userEntity;
+    // Dto를 엔티티로 변환
+    public UserEntity CreateUser(UserRegisterDto userRegisterDto) {
+            UserEntity userEntity = new UserEntity();
+            userEntity.setUsername(userRegisterDto.getUsername());
+            userEntity.setUseremail(userRegisterDto.getUseremail());
+            userEntity.setUserpassword(passwordEncoder.encode(userRegisterDto.getUserpassword()));
+            this.userRepository.save(userEntity);
+            return userEntity;
     }
-}
+
+    public UserResponseDto getUser(Long userId) {
+        UserEntity userEntity = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("사용자가 없습니다."));
+                // 예외 처리 사용자 아이디가 존재하지 않을 때
+        return new UserResponseDto(userEntity); // Entity → DTO 변환 후 반환
+    }
+    }
