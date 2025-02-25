2b42b1f6e9c5677ab8567b957cda564c790a6223
parkJJae <wgpark0830@naver.com>
2025-02-25 11:50:58 +0900

    Response Dto

---
+++ src/main/java/com/example/aiplanner/model/UserResponseDto.java
@@ -0,0 +1,18 @@
+// 회원 조회 ( 엔티티를 Dto로 변환 )
+package com.example.aiplanner.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserResponseDto {
+    private String username;
+    private String useremail;
+
+
+    public UserResponseDto(UserEntity userEntity) {
+        this.username = userEntity.getUsername();
+        this.useremail = userEntity.getUseremail();
+    }
+}
