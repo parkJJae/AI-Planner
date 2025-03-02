37ea80a6fd53a030346ce3d5cc79530cd4c30215
parkJJae <wgpark0830@naver.com>
2025-03-02 19:16:13 +0900

    컨트롤러

--- src/main/java/com/example/aiplanner/controller/UserViewController.java
+++ src/main/java/com/example/aiplanner/controller/UserViewController.java
@@ -3,18 +3,27 @@ package com.example.aiplanner.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/users")
 public class UserViewController {
 
-    @GetMapping("/register")
+    @GetMapping("users/register")
     public String showRegisterPage() {
         return "signup"; // signup.html 파일을 반환
     }
 
-    @GetMapping("/login")
+    @GetMapping("users/login")
     public String showLoginPage() {
         return "login";
+    }
+
+    @GetMapping("/planner-create")
+    public String showPlannerCreatePage() {
+        return "planner-create";
+    }
+    @GetMapping("/planner-generate")
+    public ModelAndView showPlannerForm() {
+        return new ModelAndView("planner-generate"); // templates/planner-generate.html 반환
     }
 }
