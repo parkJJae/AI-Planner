eb4da24fad433122002aea2d6a6781f099a6f842
parkJJae <wgpark0830@naver.com>
2025-03-02 19:15:51 +0900

    사용자 레파지토리

--- src/main/java/com/example/aiplanner/repository/UserRepository.java
+++ src/main/java/com/example/aiplanner/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.example.aiplanner.repository;
 
-import com.example.aiplanner.model.UserEntity;
+import com.example.aiplanner.model.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUseremail(String useremail);
 }
