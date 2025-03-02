2cb7d491ce00c8dca60937bacceca8847815053d
parkJJae <wgpark0830@naver.com>
2025-03-02 19:15:43 +0900

    사용자 등록 Dto (요청)

--- src/main/java/com/example/aiplanner/model/user/UserRegisterDto.java
+++ src/main/java/com/example/aiplanner/model/user/UserRegisterDto.java
@@ -1,8 +1,7 @@
-package com.example.aiplanner.model;
+package com.example.aiplanner.model.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
