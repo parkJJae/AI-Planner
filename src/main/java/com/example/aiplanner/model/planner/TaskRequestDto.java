9bc837fac896c554cab920d468072e5617993027
parkJJae <wgpark0830@naver.com>
2025-03-02 19:14:55 +0900

    할 일 요청 Dto

---
+++ src/main/java/com/example/aiplanner/model/planner/TaskRequestDto.java
@@ -0,0 +1,12 @@
+package com.example.aiplanner.model.planner;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskRequestDto {
+    private String taskName;
+}
