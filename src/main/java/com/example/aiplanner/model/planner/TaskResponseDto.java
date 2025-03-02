1f4163498994cbfb35cdd1100ec4fdd5baabbf2d
parkJJae <wgpark0830@naver.com>
2025-03-02 19:15:02 +0900

    할 일 응답 Dto

---
+++ src/main/java/com/example/aiplanner/model/planner/TaskResponseDto.java
@@ -0,0 +1,27 @@
+package com.example.aiplanner.model.planner;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TaskResponseDto {
+    private Long id;
+    private String taskName;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private boolean completed;
+
+    public TaskResponseDto(TaskEntity taskEntity) {
+        this.id = taskEntity.getId();
+        this.taskName = taskEntity.getTaskName();
+        this.startTime = taskEntity.getStartTime();
+        this.endTime = taskEntity.getEndTime();
+        this.completed = taskEntity.isCompleted();
+    }
+
+}
